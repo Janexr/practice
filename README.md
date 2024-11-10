fc50ad3f7c2729bf457ab78a914a5ede05d678c6
Janexr <1529890825@qq.com>
2024-11-10 23:01:57 +0800

    feat: canvas 图片着色

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # practice
 实践、记录
+src文件夹放置源代码，examples文件夹放置测试示例
+
+启动示例
+```
+npm run start-examples
+```
---
+++ src/canvas/IconImage.ts
@@ -0,0 +1,88 @@
+const iconImageCache:{[key:string]:Promise<HTMLImageElement|string>} = {};
+
+class IconImage {
+    image_?:HTMLImageElement;
+    canvas_?:HTMLCanvasElement;
+    color_ = "";
+    src_ = "";
+    constructor(src:string, color:string) {
+        this.color_ = color;
+        this.src_ = src;
+    }
+
+    async getImage() {
+        if (!this.image_) {
+            await this.load_();
+        }
+        this.replaceColor_();
+        return this.canvas_
+            ? this.canvas_.toDataURL()
+            : this.image_!;
+    }
+
+    /**
+     * @return {string|undefined} Image src.
+     */
+    getSrc() {
+        return this.src_;
+    }
+
+    /**
+     * Load not yet loaded URI.
+     */
+    async load_() {
+        if (!this.image_) {
+          this.image_ = new Image();
+        }
+
+        this.image_.src = this.src_;
+        try {
+            await this.image_.decode();
+        } catch (e) {
+            this.image_.onload = function () {
+                return true;
+            };
+            this.image_.onerror = function () {
+              throw new Error("加载图片失败！");
+            };
+        }
+    }
+
+    replaceColor_() {
+        if (!this.color_ || this.canvas_ || !this.image_) {
+            return;
+        }
+
+        const image = this.image_;
+        const canvas = document.createElement("canvas");
+        canvas.width = Math.ceil(image.width);
+        canvas.height = Math.ceil(image.height);
+
+        const ctx = canvas.getContext("2d")!;
+        ctx.drawImage(image, 0, 0);
+
+        ctx.globalCompositeOperation = "multiply";
+        ctx.fillStyle = this.color_;
+        ctx.fillRect(
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+
+        ctx.globalCompositeOperation = "destination-in";
+        ctx.drawImage(image, 0, 0);
+
+        this.canvas_ = canvas;
+    }
+}
+
+function getColorImage(src:string, color:string) {
+    const key = src + ":" + color;
+    let img = iconImageCache[key];
+    if(!img) {
+        const iconImage = new IconImage(src, color);
+        iconImageCache[key] = iconImage.getImage();
+    }
+    return iconImageCache[key];
+}
